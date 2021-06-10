a487055e7564bd865261d100c21eb1e0f5a55463
spaghoo <lincolnanderson@me.com>
2021-06-10 10:50:14 -0500

    small changes

--- Shared/views/AddItemView.swift
+++ Shared/views/AddItemView.swift
@@ -90,17 +90,6 @@ struct AddItemView: View {
                     
                 }, label: {
                     Text("Save Item")
-                        .fontWeight(.bold)
-                        .font(.title)
-                        .padding(30)
-                        .background(Color(hex: "404040"))
-                        .cornerRadius(50)
-                            .foregroundColor(Color(hex: "40cac6"))
-                        .padding(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 50)
-                                .stroke(Color(hex: "404040"), lineWidth: 5)
-                        )
                 })
                 
             }
