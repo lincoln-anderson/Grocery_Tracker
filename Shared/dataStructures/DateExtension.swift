c0c8b8b6a19edca93327faa406def9d97e7e0090
lincoln anderson <lincolnanderson@me.com>
2022-05-25 16:24:33 -0500

    crushed it

--- Shared/dataStructures/DateExtension.swift
+++ Shared/dataStructures/DateExtension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Date {
+    func isInSevenDays() -> Bool {
+        let now = Date()
+        let calendar = Calendar.current
+        let weekday = calendar.dateComponents([.weekday], from: now)
+        let startDate = calendar.startOfDay(for: now)
+        let nextWeekday = calendar.nextDate(after: now, matching: weekday, matchingPolicy: .nextTime)!
+        let endDate = calendar.date(byAdding: .day, value: 1, to: nextWeekday)!
+        return self >= startDate && self < endDate
+    }
+}
