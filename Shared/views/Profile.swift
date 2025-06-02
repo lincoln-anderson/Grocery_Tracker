9ae3b8744916389f74f8ea34b772b425519c36bb
lincoln anderson <lincolnanderson@me.com>
2025-06-02 16:09:13 -0500

    finished tab view

--- Shared/views/Profile.swift
+++ Shared/views/Profile.swift
@@ -22,19 +22,20 @@ struct ProfileView: View {
     
     var body: some View {
         let expired = groceryItems.filter { $0.expirationDate?.isExpired == true }
-        
-        HStack {
-            Text("you have \(expired.count) expired items. Would you like to mark them as uses or thrown out?").foregroundColor(.sproutGreen)
-            Button("Mark Item as Used") {
-                showingRemoveSheet = true
-            }
-            .buttonStyle(SproutsButtonStyle(color: .green))
-            .sheet(isPresented: $showingRemoveSheet) {
-                UsedGroceryView(
-                    isPresented: $showingRemoveSheet,
-                    filterString: $testString
-                )
-                .environment(\.managedObjectContext, viewContext)
+        if expired.count != 0 {
+            HStack {
+                Text("you have \(expired.count) expired items. Would you like to mark them as uses or thrown out?").foregroundColor(.sproutGreen)
+                Button("Mark Item as Used") {
+                    showingRemoveSheet = true
+                }
+                .buttonStyle(SproutsButtonStyle(color: .green))
+                .sheet(isPresented: $showingRemoveSheet) {
+                    UsedGroceryView(
+                        isPresented: $showingRemoveSheet,
+                        filterString: $testString
+                    )
+                    .environment(\.managedObjectContext, viewContext)
+                }
             }
         }
     }
